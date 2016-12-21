2da8bcf34d3c0bd1072d457ae6d9fc3c410a044e
ken <aken@matoMac-mini-2.local>
2016-12-21 10:13:21 +0800

    implement post create function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,12 +3,12 @@ class PostsController < ApplicationController
   before_filter :authenticate_user!, :only => [:new, :create]
 
   def new
-    @group = Group.find(params [:group_id])
+    @group = Group.find(params[:group_id])
     @post = Post.new
   end
 
   def create
-    @group = Group.find(params [:group_id])
+    @group = Group.find(params[:group_id])
     @post = Post.new(post_params)
     @post.group = @group
     @post.user = current_user
